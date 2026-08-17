eabca89280d70a7094434ebd93e5f627c9b020e2
Xeth Soto <xsoto@svi.com.ph>
2026-08-17 15:15:02 +1200

    feat: add page router

---
+++ js/router.js
@@ -0,0 +1,43 @@
+export class Router {
+  constructor(container, routes = {}, protectedPaths = []) {
+    this.container = container;
+    this.routes = routes;
+
+    this.load();
+
+    window.addEventListener("hashchange", () => {
+      this.load();
+    });
+  }
+
+  navigate(path) {
+    window.location.hash = path;
+  }
+
+  load() {
+    this.container.replaceChildren();
+
+    const path = window.location.hash.slice(1) || "/";
+
+    const PageClass = this.routes[path];
+
+    if (!PageClass) {
+      this.render404();
+      return;
+    }
+
+    const page = new PageClass(this.container, this);
+
+    page.render();
+  }
+
+  render404() {
+    const errorWrapper = document.createElement("div");
+    let header = document.createElement("h1");
+    header.append("404");
+    let para = document.createElement("p");
+    para.append("Page not found");
+
+    this.container.append(header, para);
+  }
+}
