15829451f099208972ae32e4a400533b2ea83154
Xeth Soto <xsoto@svi.com.ph>
2026-08-18 13:55:12 +1200

    fix: lifecycle errors due to method calls in constructor

--- js/core/viewTemplate.js
+++ js/core/viewTemplate.js
@@ -1,10 +1,4 @@
 export class ViewTemplate {
-  constructor() {
-    this.initializeElements();
-    this.setAttributes();
-    this.appendElements();
-  }
-
   initializeElements() { throw new Error("Must implement initializeElements()"); }
   setAttributes() { throw new Error("Must implement setAttributes()"); }
   appendElements() { throw new Error("Must implement appendElements()"); }
