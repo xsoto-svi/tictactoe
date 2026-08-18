76b3149666a5d5bc147215ff13c7b15ba6785106
Xeth Soto <xsoto@svi.com.ph>
2026-08-18 13:53:40 +1200

    feat: add component base class. Add board and cell components

---
+++ js/components/boardComponent.js
@@ -0,0 +1,39 @@
+import { CellComponent } from "./cellComponent.js";
+import { Component } from "./component.js";
+
+export class BoardComponent extends Component {
+  constructor(onCellClick) {
+    super();
+    this.onCellClick = onCellClick;
+
+    this.initializeElements();
+    this.setAttributes();
+    this.appendElements();
+  }
+
+  initializeElements() {
+    this.cells = [];
+    this.board = document.createElement("div");
+
+    for (let i = 0; i < 9; i++) {
+      this.cells.push(
+        new CellComponent(i, () => this.onCellClick(i))
+      );
+    }
+  }
+
+  setAttributes() {
+    this.board.classList.add("board");
+    this.cells.forEach(cell => {
+      this.board.append(cell.getHTML());
+    });
+  }
+
+  appendElements() {
+    this.componentContainer.append(this.board);
+  }
+
+  updateCell(index, symbol) {
+    this.cells[index].setValue(symbol);
+  }
+}
---
+++ js/components/component.js
@@ -0,0 +1,16 @@
+import { ViewTemplate } from "../core/viewTemplate.js";
+
+export class Component extends ViewTemplate {
+  constructor() {
+    super();
+    this.componentContainer = document.createElement("div");
+  }
+
+  getHTML() {
+    return this.componentContainer;
+  }
+
+  attachEvents() {
+    throw new Error("attachEvents method must be implemented");
+  }
+}
