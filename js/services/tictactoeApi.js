71ea3f294dd27865dc796de5c8504c625f22a92c
Xeth Soto <xsoto@svi.com.ph>
2026-08-20 12:16:07 +1200

    feat: add options to apis

--- js/services/tictactoeApi.js
+++ js/services/tictactoeApi.js
@@ -4,24 +4,24 @@ export class TicTacToeApi extends ApiClient {
   constructor() {
     super("http://localhost:8080/tictactoe/tictactoeserver");
   }
-  
-  createGame(roomCode) {
+
+  createGame(roomCode, options = {}) {
     return this.get(`/createGame?key=${roomCode}`);
   }
 
-  checkRoomStatus(roomCode) {
+  checkRoomStatus(roomCode, option = {}) {
     return this.get(`/check?key=${roomCode}`);
   }
 
-  updateToAddMove(roomCode, symbol, x, y) {
+  updateToAddMove(roomCode, symbol, x, y, options = {}) {
     return this.get(`/move?key=${roomCode}&tile=${symbol}&y=${y}&x=${x}`);
   }
 
-  boardStatus(roomCode) {
+  boardStatus(roomCode, options = {}) {
     return this.get(`/board?key=${roomCode}`);
   }
 
-  resetGame(roomCode) {
-    return this.get(`/reset?key=${roomCode}`);
+  resetGame(roomCode, options = {}) {
+    return this.get(`/reset?key=${roomCode}`, options);
   }
 }
