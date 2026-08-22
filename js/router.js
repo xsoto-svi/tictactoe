298acf33649bb6b0c4577d4e80aef5ec29d87c60
xthst <sotoxeth1@gmail.com>
2026-08-22 19:51:26 +0800

    feat: removed path routing

--- js/router.js
+++ js/router.js
@@ -1,36 +1,48 @@
+import { HomePage } from "./pages/homePage.js";
+import { HowToPlayPage } from "./pages/howToPlayPage.js";
+import { LobbyPage } from "./pages/lobbyPage.js";
+import { GamePage } from "./pages/gamePage.js";
+
 export class Router {
+  static Screens = {
+    HOME: "HOME",
+    HOW_TO_PLAY: "HOW_TO_PLAY",
+    LOBBY: "LOBBY",
+    GAME: "GAME"
+  }
+
   constructor(appContainer, routes = {}, tictactoeApi, gameState) {
     this.appContainer = appContainer;
     this.routes = routes;
     this.tictactoeApi = tictactoeApi;
     this.gameState = gameState;
 
-    this.load();
-
-    window.addEventListener("hashchange", () => {
-      this.load();
-    });
+    this.navigate("HOME");
   }
 
-  navigate(path) {
-    window.location.hash = path;
-  }
-
-  load() {
+  navigate(screenName, params = {}) {
     this.appContainer.replaceChildren();
 
-    const path = window.location.hash.slice(1) || "/";
+    let activePage;
 
-    const PageClass = this.routes[path];
-
-    if (!PageClass) {
-      this.render404();
-      return;
+    switch (screenName) {
+      case Router.Screens.HOME:
+        activePage = new HomePage(this.appContainer, this);
+        break;
+      case Router.Screens.HOW_TO_PLAY:
+        activePage = new HowToPlayPage(this.appContainer, this);
+        break;
+      case Router.Screens.LOBBY:
+        activePage = new LobbyPage(this.appContainer, this, this.tictactoeApi, this.gameState);
+        break;
+      case Router.Screens.GAME:
+        activePage = new GamePage(this.appContainer, this, this.tictactoeApi, this.gameState);
+        break;
+      default:
+        activePage = new HomePage(this.appContainer, this);
     }
-
-    const page = new PageClass(this.appContainer, this, this.tictactoeApi, this.gameState);
-
-    page.render();
+    
+    activePage.render();
   }
 
   render404() {
