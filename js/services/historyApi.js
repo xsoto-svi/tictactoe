8d9c617a8bdaa96fe8519c6ab1f7ac8e54595681
Xeth Soto <xsoto@svi.com.ph>
2026-09-04 11:23:11 +0800

    feat: add HistoryApi service for managing game history and player data

---
+++ js/services/historyApi.js
@@ -0,0 +1,32 @@
+import { ApiClient } from "./apiClient.js";
+
+export class HistoryApi extends ApiClient {
+  constructor() {
+    super("http://localhost:8080/tictactoe-webservice/api");
+  }
+
+  // Placeholder APIs for History
+  getAllRooms() {
+    return this.get(`/rooms`);
+  }
+
+  getAllPlayers() {
+    return this.get(`/player/all`);
+  }
+
+  getGamesByRoom(roomId) {
+    return this.get(`/room/${roomId}`);
+  }
+
+  getGamesByPlayer(playerName) {
+    return this.get(`/player/${playerName}/games`);
+  }
+
+  getGameDetails(gameId) {
+    return this.get(`/game/${gameId}`);
+  }
+
+  saveMove(body) {
+    return this.post(`/game/save`, body);
+  }
+}
